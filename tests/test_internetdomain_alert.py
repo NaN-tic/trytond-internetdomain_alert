8cff4ba8c803745eaf3f22b382e44569cdea6ca5
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-06 11:13:37 +0100

    Cleanup tests

--- tests/test_internetdomain_alert.py
+++ tests/test_internetdomain_alert.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
-#This file is part internetdomain_alert module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part internetdomain_alert module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
 
 
 class InternetdomainAlertTestCase(unittest.TestCase):
-    '''
-    Test Internet Domain Alert module.
-    '''
+    'Test Internet Domain Alert module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('internetdomain_alert')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -35,6 +23,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         InternetdomainAlertTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
