98e1eab37fc418f82967c390b200ab916ae7af4d
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:16:46 +0100

    Cleanup tests

--- internetdomain_alert.py
+++ internetdomain_alert.py
@@ -1,6 +1,6 @@
-#This file is part internetdomain_alert module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part internetdomain_alert module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.transaction import Transaction
 from trytond.pool import Pool, PoolMeta
 
