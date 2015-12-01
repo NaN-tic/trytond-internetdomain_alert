a7f2ef9d16d02f7ca289bbed5245e084da235032
resteve <resteve@zikzakmedia.com>
2015-12-01 11:09:24 +0100

    Tests

--- tests/test_internetdomain_alert.py
+++ tests/test_internetdomain_alert.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part internetdomain_alert module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the internetdomain_alert module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class InternetdomainAlertTestCase(unittest.TestCase):
-    'Test Internet Domain Alert module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('internetdomain_alert')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class InternetdomainAlertTestCase(ModuleTestCase):
+    'Test Internetdomain Alert module'
+    module = 'internetdomain_alert'
 
 
 def suite():
